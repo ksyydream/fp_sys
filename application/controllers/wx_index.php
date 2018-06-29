94bf5d59d622d45d4549a6a25f784376d0530628
double_yang <123456>
2018-06-29 11:15:31 +0800

    20180614

--- application/controllers/wx_index.php
+++ application/controllers/wx_index.php
@@ -16,7 +16,7 @@ class Wx_index extends Wx_controller {
         parent::__construct();
         $this->load->model('wx_index_model');
         $this->load->model('wx_salesman_model');
-        $ignore_methods = array('logout', 'change_pwd', 'save_change_pwd', 'submit_login','main','pg_list');
+        $ignore_methods = array('logout', 'change_pwd', 'save_change_pwd', 'submit_login','main','pg_list','calculator');
         if($this->session->userdata('wx_user_id') && !in_array($this->uri->segment(2), $ignore_methods)){
             if($this->session->userdata('wx_role_id') >= 1){
                 redirect('wx_salesman');
