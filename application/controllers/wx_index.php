b964ee9ad86bcad39c3f774eee457fdcdf87e2d6
double_yang <123456>
2018-06-29 13:40:11 +0800

    20180629

--- application/controllers/wx_index.php
+++ application/controllers/wx_index.php
@@ -16,7 +16,7 @@ class Wx_index extends Wx_controller {
         parent::__construct();
         $this->load->model('wx_index_model');
         $this->load->model('wx_salesman_model');
-        $ignore_methods = array('logout', 'change_pwd', 'save_change_pwd', 'submit_login','main','pg_list','calculator');
+        $ignore_methods = array('logout', 'change_pwd', 'save_change_pwd', 'submit_login','main','pg_list','calculator','calculator_res');
         if($this->session->userdata('wx_user_id') && !in_array($this->uri->segment(2), $ignore_methods)){
             if($this->session->userdata('wx_role_id') >= 1){
                 redirect('wx_salesman');
@@ -89,4 +89,10 @@ class Wx_index extends Wx_controller {
             $this->assign('pagination_url','/wx_index/calculator/');
             $this->display('estimate/calculator.html');
         }
+
+    public function calculator_res(){
+        $data = $this->input->get();
+        $this->assign('data',$data);
+        $this->display('estimate/calculator_res.html');
+    }
 }
