fad1950735d0b91527d914a64eaa9940f220f3b8
ksyydream <249499109@qq.com>
2018-09-02 14:46:50 +0800

    新页面

--- application/controllers/wx_index.php
+++ application/controllers/wx_index.php
@@ -16,7 +16,7 @@ class Wx_index extends Wx_controller {
         parent::__construct();
         $this->load->model('wx_index_model');
         $this->load->model('wx_salesman_model');
-        $ignore_methods = array('logout', 'change_pwd', 'save_change_pwd', 'submit_login','main','pg_list','calculator','calculator_res','api_get_xiaoqu_list','pg_list_test');
+        $ignore_methods = array('logout', 'change_pwd', 'save_change_pwd', 'submit_login','main','pg_list','calculator','calculator_res','api_get_xiaoqu_list','pg_list_test','pg_list_new');
         if($this->session->userdata('wx_user_id') && !in_array($this->uri->segment(2), $ignore_methods)){
             if($this->session->userdata('wx_role_id') >= 1){
                 redirect('wx_salesman');
@@ -86,6 +86,10 @@ class Wx_index extends Wx_controller {
 
     public function pg_list_test(){
         $this->display('estimate/estimate_test.html');
+    }
+
+    public function pg_list_new(){
+        $this->display('estimate/estimate-new.html');
     }
 
     public function api_get_xiaoqu_list(){
