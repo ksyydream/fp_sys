3ab7b38f53591df1d3c6bb933fba3e2a5c1f4e4c
ksyydream <249499109@qq.com>
2018-09-05 13:18:26 +0800

    修改

--- application/controllers/wx_controller.php
+++ application/controllers/wx_controller.php
@@ -20,35 +20,23 @@ class Wx_controller extends MY_Controller
         $this->wxconfig['appid']=$this->config->item('appid');
         $this->wxconfig['appsecret']=$this->config->item('appsecret');
         //var_dump($this->wxconfig);
-        //if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false ) {
+        if ( strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false ) {
             if(!$this->session->userdata('openid')){
-                $appid = $this->wxconfig['appid'];
-                $secret = $this->wxconfig['appsecret'];
-                if(empty($_GET['code'])){
-                    $url = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"]; //这是要回调地址可以有别的写法
-                    redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appid}&redirect_uri={$url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect");
-                    //重定向到以上网址,这是微信给的固定地址.必须格式一致
-                }else{
-                    //回调成功,获取code,再做请求,获取openid
-                    $j_access_token=file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appid}&secret={$secret}&code={$_GET['code']}&grant_type=authorization_code");
-                    $a_access_token=json_decode($j_access_token,true);
-                    $access_token=$a_access_token["access_token"];//虽然这里 也获取了一个access_token,但是和获取用户详情,还有发送模板信息所使用的access_token不同
-                    $openid=$a_access_token["openid"];
-                    $this->session->set_userdata('openid', $openid);
-                    $res = $this->sys_model->check_openid($openid);
-                }
+                $this->get_openid();
             }else{
                 $res = $this->sys_model->check_openid($this->session->userdata('openid'));
             }
-
-        //}else{
-            //$openid = 'oFzKgwbFEyC40jU6bS_HQ5sxM4X8';
-            //$this->session->set_userdata('openid', $openid);
-            //$res = $this->sys_model->check_openid($openid);
-        //}
+        }else{
+            $openid = 'oFzKgwbFEyC40jU6bS_HQ5sxM4X8';
+            $this->session->set_userdata('openid', $openid);
+            $res = $this->sys_model->check_openid($openid);
+        }
         if(!$check_person_check = $this->sys_model->check_person()){
             $person_info = $this->getUserInfoById($this->session->userdata('openid'));
             if($person_info){
+                if($person_info['subscribe'] != 1){
+                    $this->get_openid();
+                }
                 $this->sys_model->save_person($person_info);
             }
         }
@@ -59,6 +47,29 @@ class Wx_controller extends MY_Controller
             $this->cismarty->assign('role_name',$this->session->userdata('wx_role_name'));
         }
 
+    }
+
+    /**
+     * 获取微信的openid
+     * @author yaobin <bin.yao@thmarket.cn>
+     * @date 2017-12-27
+     */
+    public function get_openid(){
+            $appid = $this->wxconfig['appid'];
+            $secret = $this->wxconfig['appsecret'];
+            if(empty($_GET['code'])){
+                $url = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"]; //这是要回调地址可以有别的写法
+                redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid={$appid}&redirect_uri={$url}&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect");
+                //重定向到以上网址,这是微信给的固定地址.必须格式一致
+            }else{
+                //回调成功,获取code,再做请求,获取openid
+                $j_access_token=file_get_contents("https://api.weixin.qq.com/sns/oauth2/access_token?appid={$appid}&secret={$secret}&code={$_GET['code']}&grant_type=authorization_code");
+                $a_access_token=json_decode($j_access_token,true);
+                $access_token=$a_access_token["access_token"];//虽然这里 也获取了一个access_token,但是和获取用户详情,还有发送模板信息所使用的access_token不同
+                $openid=$a_access_token["openid"];
+                $this->session->set_userdata('openid', $openid);
+                $res = $this->sys_model->check_openid($openid);
+            }
     }
 
     //重载smarty方法assign
