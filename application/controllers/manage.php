5d751d7031d0264f13a030ef2d047b1773e20d99
zhouxiaohui123 <zxh1989117>
2018-09-28 17:17:19 +0800

    1111

--- application/controllers/manage.php
+++ application/controllers/manage.php
@@ -91,6 +91,10 @@ class Manage extends MY_Controller {
 		$this->load->view('manage/index.php');
 	}
 
+    public function search-credit()
+	{
+		$this->load->view('manage/search-credit.php');
+	}
 	/**
 	 * 公司信息
 	 */
