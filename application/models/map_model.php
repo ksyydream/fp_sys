2278f91b53bdae5c1c9db36f65513d5ff3402aa5
ksyydream <249499109@qq.com>
2019-05-06 21:52:57 +0800

    成绩查询

--- application/models/map_model.php
+++ application/models/map_model.php
@@ -123,26 +123,26 @@ class Map_model extends MY_Model
         }
     }
 
-    public function get_result_yy(){
-        $res = array('status' => 1, 'result' => [], 'msg' => '');
-        $keyword = trim($this->input->post('keyword'));
-        $this->db->select()->from('exam_result');
-        if($keyword){
-            $this->db->where('ticket', $keyword);
-            $this->db->or_where('code', $keyword);
-        }else{
-            $res['status'] = -1;
-            $res['msg'] = '请输入信息再查询';
-            return $res;
-        }
-        $data = $this->db->get()->row_array();
-        //die(var_dump($this->db->last_query()));
-        if(!$data){
-            $res['status'] = -1;
-            $res['msg'] = '未找到信息!';
-            return $res;
-        }
-        $res['result'] = $data;
-        return $res;
-    }
+    //public function get_result_yy(){
+    //    $res = array('status' => 1, 'result' => [], 'msg' => '');
+    //    $keyword = trim($this->input->post('keyword'));
+    //    $this->db->select()->from('exam_result');
+    //    if($keyword){
+    //        $this->db->where('ticket', $keyword);
+    //        $this->db->or_where('code', $keyword);
+    //    }else{
+    //        $res['status'] = -1;
+    //        $res['msg'] = '请输入信息再查询';
+    //        return $res;
+    //    }
+    //    $data = $this->db->get()->row_array();
+    //    //die(var_dump($this->db->last_query()));
+    //    if(!$data){
+    //        $res['status'] = -1;
+    //        $res['msg'] = '未找到信息!';
+    //        return $res;
+    //    }
+    //    $res['result'] = $data;
+    //    return $res;
+    //}
 }
