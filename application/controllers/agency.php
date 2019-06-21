82fcc2350f7f1dc0320cab80a1bd9338f70fc967
ksyydream <249499109@qq.com>
2019-06-21 18:24:34 +0800

    新页面上传

---
+++ application/controllers/agency.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bin.shen
+ * Date: 6/2/16
+ * Time: 21:22
+ */
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Agency extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        ini_set('date.timezone','Asia/Shanghai');
+    }
+
+    //重载smarty方法assign
+    public function assign($key,$val) {
+        $this->cismarty->assign($key,$val);
+    }
+
+    //重载smarty方法display
+    public function display($html) {
+        $this->cismarty->display($html);
+    }
+
+    public function inquiry(){
+        $this->display('agency/index.html');
+    }
+}
+
