9947e2549c3b661b81c62acaef6069323cb61b67
ksyydream <249499109@qq.com>
2019-06-28 22:27:09 +0800

    ali test service

--- application/controllers/agency.php
+++ application/controllers/agency.php
@@ -30,14 +30,9 @@ class Agency extends CI_Controller
         $this->display('agency/index.html');
     }
 
-    public function test_sms(){
-        $this->load->model('sms_model');
-        $ali_templateCode = $this->config->item('ali_templateCode');
-        $res = $this->sms_model->sendSmsByAliyun('18914970292', '房猫服务中心', '8888', $ali_templateCode['1']);
-        die(var_dump($res));
-    }
 
     public function sendSms(){
+        die('test....');
         $this->load->model('sms_model');
         $ali_templateCode = $this->config->item('ali_templateCode');
         $res = $this->sms_model->sendSms('18914970292', '房猫服务中心', '1111', $ali_templateCode['1']);
--- application/models/sms_model.php
+++ application/models/sms_model.php
@@ -30,50 +30,6 @@ class Sms_model extends MY_Model
         parent::__destruct();
     }
 
-    public function sendSmsByAliyun($mobile, $smsSign, $smsParam, $templateCode)
-    {
-        include_once './application/libraries/api_sdk/lib/Core/Config.php';
-        include_once './application/libraries/api_sdk/lib/Api/Sms/Request/V20170525/SendSmsRequest.php';
-
-        $accessKeyId = $this->config->item('accessKeyID');
-        $accessKeySecret = $this->config->item('accessKeySecret');
-
-        //短信API产品名
-        $product = "Dysmsapi";
-        //短信API产品域名
-        $domain = "dysmsapi.aliyuncs.com";
-        //暂时不支持多Region
-        $region = "cn-hangzhou";
-
-        //初始化访问的acsCleint
-        $profile = \Aliyun\Core\Profile\DefaultProfile::getProfile($region, $accessKeyId, $accessKeySecret);
-        \Aliyun\Core\Profile\DefaultProfile::addEndpoint("cn-hangzhou", "cn-hangzhou", $product, $domain);
-        $acsClient= new \Aliyun\Core\DefaultAcsClient($profile);
-
-        $request = new \Aliyun\Api\Sms\Request\V20170525\SendSmsRequest;
-        //必填-短信接收号码
-        $request->setPhoneNumbers($mobile);
-        //必填-短信签名
-        $request->setSignName($smsSign);
-        //必填-短信模板Code
-        $request->setTemplateCode($templateCode);
-        // 短信模板中字段的值
-        $smsParam = json_encode($smsParam, JSON_UNESCAPED_UNICODE);
-        //选填-假如模板中存在变量需要替换则为必填(JSON格式)
-        $request->setTemplateParam($smsParam);
-        //选填-发送短信流水号
-        //$request->setOutId("1234");
-        //die(var_dump($request));
-        //发起访问请求
-        $resp = $acsClient->getAcsResponse($request);
-        //短信发送成功返回True，失败返回false
-        if ($resp && $resp->Code == 'OK') {
-            return array('status' => 1, 'msg' => $resp->Code);
-        } else {
-            return array('status' => -1, 'msg' => $resp->Message . ' subcode:' . $resp->Code);
-        }
-    }
-
     public static function getAcsClient($accessKeyId_config, $accessKeySecret_config) {
         //产品名称:云通信短信服务API产品,开发者无需替换
         $product = "Dysmsapi";
