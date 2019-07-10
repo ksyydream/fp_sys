7bcd9008eb349399b1586498bbd69feb66a7eb1f
ksyydream <249499109@qq.com>
2019-07-10 21:47:28 +0800

    图片上传

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -614,14 +614,14 @@ class MY_Model extends CI_Model{
         $accessToken = $this->get_token($app,$appsecret);
         $url = "http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=".$accessToken."&media_id=".$media_id;
 
-        if (is_readable('./uploadfiles/' . $file) == false) {
-            mkdir('./uploadfiles/finance', 0777, true);
+        if (is_readable('./upload_files/' . $file) == false) {
+            mkdir('./upload_files/finance', 0777, true);
         }
-        if (is_readable('./uploadfiles/'.$file.'/'.$finance_num) == false) {
-            mkdir('./uploadfiles/finance/'.$finance_num, 0777, true);
+        if (is_readable('./upload_files/'.$file.'/'.$finance_num) == false) {
+            mkdir('./upload_files/finance/'.$finance_num, 0777, true);
         }
         $file_name = date('YmdHis').rand(1000,9999).'.jpg';
-        $targetName = './uploadfiles/'.$file.'/'.$finance_num.'/'.$file_name;
+        $targetName = './upload_files/'.$file.'/'.$finance_num.'/'.$file_name;
         //file_put_contents('/var/yy.txt', $url);
 
         $ch = curl_init($url); // 初始化
