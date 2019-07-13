64dbb61c285cb401000d1c78f5e539b6afe350d5
ksyydream <249499109@qq.com>
2019-07-13 22:34:10 +0800

    fix

--- application/controllers/wx_members.php
+++ application/controllers/wx_members.php
@@ -18,6 +18,7 @@ class Wx_members extends Wx_controller {
         $this->load->model('wx_index_model');
         $this->load->model('wx_members_model');
         $this->load->model('foreclosure_model');
+        $this->load->model('wx_users_model');
         if($this->session->userdata('wx_class') != 'members' || !$this->session->userdata('wx_m_id') ){
             redirect('wx_index/logout');
         }
@@ -88,7 +89,7 @@ class Wx_members extends Wx_controller {
                 redirect('wx_members/foreclosure_list'); //不是自己的工作单,就直接回到首页
             }
             if($f_info['status'] != 2){
-                redirect('wx_members/foreclosure_list'); ////如果工作单不是待审核,就到详情页面
+                redirect('wx_members/foreclosure_detail1/' . $f_id); ////如果工作单不是待审核,就到详情页面
             }
         }
         return true;
@@ -207,6 +208,50 @@ class Wx_members extends Wx_controller {
         $this->assign('f_info', $f_info);
         $this->assign('credit_img_list', $credit_img_list);
         $this->display('members/foreclosure/step6.html');
+    }
+
+    private function foreclosure_detail_common($f_id = 0){
+        $f_info = $this->foreclosure_model->get_foreclosure($f_id);
+        if(!$f_info){
+            redirect('wx_users/index'); //不是自己的工作单,就直接回到首页
+        }
+        $user_info = $this->wx_users_model->get_user_info($f_info['user_id']);
+        $this->assign('f_info', $f_info);
+        $this->assign('user_info', $user_info);
+        //获取直接对接 业务员的信息
+        $m_info = $this->m_info;
+        $manger_info = $this->wx_members_model->get_member_info($f_info['m_id']);
+        if($f_info['m_id'] != $m_info['m_id'] && $manger_info['parent_id'] != $m_info['m_id']){
+            redirect('wx_members/foreclosure_list'); //不是自己的工作单,就直接回到首页
+        }
+        return true;
+    }
+    //赎楼详情页 1
+    public function foreclosure_detail1($f_id = 0){
+        $this->foreclosure_detail_common($f_id);
+        $this->display('members/foreclosure/detail1.html');
+    }
+
+    //赎楼详情页 身份证
+    public function foreclosure_detail3($f_id = 0){
+        $this->foreclosure_detail_common($f_id);
+        $this->display('members/foreclosure/detail3.html');
+    }
+
+    //赎楼详情页 房产证
+    public function foreclosure_detail4($f_id = 0){
+        $this->foreclosure_detail_common($f_id);
+        $property_img_list = $this->foreclosure_model->get_property_img($f_id);
+        $this->assign('property_img_list', $property_img_list);
+        $this->display('members/foreclosure/detail4.html');
+    }
+
+    //赎楼详情页 房产证
+    public function foreclosure_detail5($f_id = 0){
+        $this->foreclosure_detail_common($f_id);
+        $credit_img_list = $this->foreclosure_model->get_credit_img($f_id);
+        $this->assign('credit_img_list', $credit_img_list);
+        $this->display('members/foreclosure/detail5.html');
     }
 
 
