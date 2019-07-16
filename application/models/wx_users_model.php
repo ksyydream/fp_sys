0503d19c991a6651622451ac8c7c1069d955a518
ksyydream <249499109@qq.com>
2019-07-16 22:06:37 +0800

    门店 个人信息页面

--- application/models/wx_users_model.php
+++ application/models/wx_users_model.php
@@ -22,5 +22,14 @@ class Wx_users_model extends MY_Model
         return $this->db->select()->from('users')->where('user_id', $user_id)->get()->row_array();
     }
 
-
+    public function get_user_info4region($user_id){
+        $this->db->select('us.*,r1.name r1_name,r2.name r2_name,r3.name r3_name,r4.name r4_name');
+        $this->db->from('users us');
+        $this->db->join('region r1', 'us.province = r1.id', 'left');
+        $this->db->join('region r2', 'us.city = r2.id', 'left');
+        $this->db->join('region r3', 'us.district = r3.id', 'left');
+        $this->db->join('region r4', 'us.twon = r4.id', 'left');
+        $ret = $this->db->where(array('us.user_id' => $user_id))->get()->row_array();
+        return $ret;
+    }
 }
