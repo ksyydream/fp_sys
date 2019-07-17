38fbf3bf2430b44a59a425b8ce26c4bc9da3afe5
ksyydream <249499109@qq.com>
2019-07-17 09:11:10 +0800

    转发

--- application/controllers/wx_members.php
+++ application/controllers/wx_members.php
@@ -47,8 +47,8 @@ class Wx_members extends Wx_controller {
         $this->load->library('wxjssdk_th',array('appid' => $this->config->item('appid'), 'appsecret' => $this->config->item('appsecret')));
         $access_token = $this->wxjssdk_th->wxgetAccessToken();
         $img_url = $this->get_or_create_ticket($access_token, 'QR_STR_SCENE', $this->m_info['invite_code']);
-        $this->cismarty->assign('img_url',$img_url);
-        $this->cismarty->display('members/wx_qr_code.html');
+        $this->assign('img_url',$img_url);
+        $this->display('members/wx_qr_code.html');
     }
 
     //赎楼列表
